e440eaaabd5e49a2a9f97730d86a311faf075ebb
TheFlyer1983 <theflyer1983@gmail.com>
2023-10-20 12:31:32 +0100

    Adding tailwind

--- components/StyledSelect.vue
+++ components/StyledSelect.vue
@@ -42,3 +42,18 @@ const v = computed({
     <slot />
   </select>
 </template>
+
+<style scoped>
+.styled-select {
+  background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%23999999'><polygon points='0,0 100,0 50,50'/></svg>")
+    no-repeat;
+  background-size: 10px;
+  background-position: calc(100% - 10px) 65%;
+  background-repeat: no-repeat;
+  background-color: #efefef;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  position: relative;
+}
+</style>
